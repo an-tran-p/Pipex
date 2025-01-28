49b8582ed0f73f33cdca125e5bbe409860c85645
An Tran <atran@c2r2p2.hive.fi>
2025-01-28 20:32:10 +0200

    add some progress

--- pipex.c
+++ pipex.c
@@ -6,20 +6,20 @@
 /*   By: atran <atran@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 18:02:26 by atran             #+#    #+#             */
-/*   Updated: 2025/01/06 18:38:59 by atran            ###   ########.fr       */
+/*   Updated: 2025/01/28 19:54:31 by atran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void    error_handling(int flag)
+void	error_handling(int flag)
 {
-    if (flag == 1)
-        ft_printf("Error: Program needs 4 arguments: <file1> <cmd1> <cmd2> <file2>\n");
+	if (flag == 1)
+		ft_printf("Error: 4 arguments needed <file1> <cmd1> <cmd2> <file2>\n");
 }
 
-int main(int argc, char **argv, char **env)
+int	main(int argc, char **argv, char **envp)
 {
-    if (argc != 5)
-        error_handling(1);
+	if (argc != 5)
+		error_handling(1);
 }
--- pipex.h
+++ pipex.h
@@ -6,14 +6,16 @@
 /*   By: atran <atran@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/06 17:45:16 by atran             #+#    #+#             */
-/*   Updated: 2025/01/06 18:29:25 by atran            ###   ########.fr       */
+/*   Updated: 2025/01/28 14:36:50 by atran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include "libft/libft.h"
+# include "Libft/libft.h"
 # include "Printf/ft_printf.h"
+# include <unistd.h>
+# include <fcntl.h>
 
 #endif
